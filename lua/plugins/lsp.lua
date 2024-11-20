06409bfff3a64ee9747baf34984dae52f89456a9
low-key <lokesh@low-key.me>
2024-11-20 23:35:19 +0530

    Remove lua ls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -7,35 +7,6 @@ return {
       'williamboman/mason.nvim',
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-
-      -- Configure LuaLS for editing Neovim config
-      {
-        {
-          'folke/lazydev.nvim',
-          ft = 'lua', -- only load on lua files
-          opts = {
-            library = {
-              -- See the configuration section for more details
-              -- Load luvit types when the `vim.uv` word is found
-              { path = 'luvit-meta/library', words = { 'vim%.uv' } },
-            },
-          },
-        },
-        { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
-        { -- optional completion source for require statements and module annotations
-          'hrsh7th/nvim-cmp',
-          opts = function(_, opts)
-            opts.sources = opts.sources or {}
-            table.insert(opts.sources, {
-              name = 'lazydev',
-              group_index = 0, -- set group index to 0 to skip loading LuaLS completions
-            })
-          end,
-        },
-      },
-
-      -- Useful status updates for LSP.
-      { 'j-hui/fidget.nvim', opts = {} },
     },
     config = function()
       -- Mason
@@ -131,24 +102,6 @@ return {
 
       -- Enable the following language servers
       local servers = {
-        -- Lua language server
-        lua_ls = {
-          settings = {
-            Lua = {
-              runtime = { version = 'LuaJIT' },
-              workspace = {
-                checkThirdParty = false,
-                library = {
-                  '${3rd}/luv/library',
-                  unpack(vim.api.nvim_get_runtime_file('', true)),
-                },
-              },
-              completion = {
-                callSnippet = 'Replace',
-              },
-            },
-          },
-        },
         -- Docker language server
         docker_compose_language_service = {},
         -- YAML language server
